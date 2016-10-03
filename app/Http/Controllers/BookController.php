088b39d35a637a12d227e1c79eaf2b2f6f9803cc
Miranda Mu <mirandamu@gmail.com>
2016-10-03 18:41:22 -0400

    updating from lesson 5

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return 'Hi from the book controller!';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $view  = '<form method="POST" action="/books/create">';
+        $view .= csrf_field(); # This will be explained more later
+        $view .= '<label>Title: <input type="text" name="title"></label>';
+        $view .= '<input type="submit">';
+        $view .= '</form>';
+        return $view;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('books/create', function() {
+    return "Process adding the book...";
+    dd(Request::all());
+});
+
+/* Route::get('/books/{title?}', function($title = '') {
+    if($title == '') {
+        return 'You did not include a title.';
+    }
+    return 'You requested the book: '.$title;
+}); */
+
+Route::get('/books', 'BookController@index')->name('books.index');
+Route::get('/books/create', 'BookController@create')->name('books.create');
+Route::post('/books', 'BookController@store')->name('books.store');
+Route::get('/books/{book}', 'BookController@show')->name('books.show');
+Route::get('/books/{book}/edit', 'BookController@edit')->name('books.edit');
+Route::put('/books/{book}', 'BookController@update')->name('books.update');
+Route::delete('/books/{book}', 'BookController@destroy')->name('books.destroy');
+
+Route::get('/example', function() {
+    return App::environment();
+});
